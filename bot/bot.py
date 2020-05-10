351a0c1a8f97b855eaea70367d0912a538e5f79d
jespetius74 <jeppe.kuula@gmail.com>
2020-05-10 17:05:57 +0300

    bot replies, likes and retweets

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,49 @@
-#haetaan twitteravaimet piilosta
-from config import getApi
+import tweepy
 import os
+import time
+from dotenv import load_dotenv
+load_dotenv()
 
-#api
-api = getApi()
+# Tunnistusobjektin luominen
+auth = tweepy.OAuthHandler(os.getenv('twitter_consumer_key'),
+                       os.getenv('twitter_consumer_secret'))
+# Access tokenien asettaminen
+auth.set_access_token(os.getenv('twitter_access_token'),
+                      os.getenv('twitter_access_token_secret'))
+# API-objektin luominen ja auth informaation puskeminen
+api = tweepy.API(auth)
+FILE_NAME = 'last_seen.txt'
 
-#yksi tweetti
-def postStatus(update):
-    status = api.PostUpdate(update)
-    print(status)
+#lähetä yksi tweetti
+#api.update_status('Camoon #opetuslapaset')
 
-postStatus("Hi, im a bot")
+
+
+#hae viimeksi luettu tweetti .txt
+def read_last_seen(FILE_NAME):
+    file_read = open(FILE_NAME, 'r')
+    last_seen_id = int(file_read.read().strip())
+    file_read.close()
+    return last_seen_id
+
+#ota talteen viimeisin tweetti
+def store_last_seen(FILE_NAME, last_seen_id):
+    file_write = open(FILE_NAME, 'w')
+    file_write.write(str(last_seen_id))
+    file_write.close()
+    return
+    
+#etsi tweetit jossa on mainittu api, eli twitterkäyttäjä ja vastaa
+def reply():
+    tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode='extended')
+    for tweet in reversed(tweets):
+        if "#opetuslapaset" in tweet.full_text.lower():
+            print(str(tweet.id)+ ' - ' + tweet.full_text) 
+            api.update_status("@" + tweet.user.screen_name + " Botti vastaa, tykkää ja retweettaa :)", tweet.id)
+            api.create_favorite(tweet.id)
+            api.retweet(tweet.id)
+            store_last_seen(FILE_NAME, tweet.id)
+
+while True:
+    reply()
+    time.sleep(2)
---
+++ bot/print.py
@@ -0,0 +1,8 @@
+FILE_NAME = 'last_seen.txt'
+
+def read_last_seen(FILE_NAME):
+    file_read = open(FILE_NAME, 'r')
+    last_seen_id = int(file_read.read().strip())
+    file_read.close()
+    print(last_seen_id)
+    return last_seen_id
